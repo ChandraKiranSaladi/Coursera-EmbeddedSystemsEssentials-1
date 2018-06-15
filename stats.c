7a4f47ba356df9665ce68b7440edcf9bbcaccb5f
Chandra Kiran Saladi <kiranck18@gmail.com>
2018-06-14 21:06:26 -0500

    Adding the code outline and documentation for C1M1

--- stats.c
+++ stats.c
@@ -8,9 +8,7 @@
  * misuse of this material. 
  *
  *****************************************************************************/
-
-
-/*
+/**
  * @file stats.c
  * @brief Simple Program to print the statistics of the given data such as 
  					- Maximum
@@ -22,24 +20,50 @@
  *
  * @author Chandra Kiran Saladi
  * @date 06/13/2018
+ *
  */
-#include<stdio.h>
+
+
+// Header Files. 
+#include <stdio.h>
 #include "stats.h"
 
 /* Size of the Data Set */
 #define SIZE (40)
 
-void main() {
+int main(){
 
-  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
-                              114, 88,   45,  76, 123,  87,  25,  23,
-                              200, 122, 150, 90,   92,  87, 177, 244,
-                              201,   6,  12,  60,   8,   2,   5,  67,
-                                7,  87, 250, 230,  99,   3, 100,  90};
+return 0;
+}
 
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
+unsigned char find_mean(unsigned char * temp, int size){
+	
+}
+
+unsigned char find_maximum(unsigned char * temp, int size){
+
 
 }
 
-/* Add other Implementation File Code Here */
+unsigned char find_minimum(unsigned char * temp, int size){
+
+}
+
+void print_array(unsigned char * temp, int size){
+
+}
+
+unsigned char find_median(unsigned char * temp, int size){
+
+
+}
+
+void sort_array(unsigned char * temp, int size){
+
+}
+
+void print_statistics(unsigned char * temp, int size, unsigned char maximum,
+							 unsigned char minimum, unsigned char mean, unsigned char median){
+
+}
+
--- stats.h
+++ stats.h
@@ -7,7 +7,6 @@
  * software. Alex Fosdick and the University of Colorado are not liable for any
  * misuse of this material. 
  *
-
  *****************************************************************************/
 /**
  * @file stats.h 
@@ -17,31 +16,101 @@
  					- Mean
  					- Median
  *
+ * <Add Extended Description Here>
  *
  * @author Chandra Kiran Saladi
  * @date 06/13/2018
  *
  */
-
-
-
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
-
-/**
- * @brief <Add Brief Description of Function Here>
+/**************************************************************
+ * @brief Prints the array to the screen
+ *		Takes an array and size as input. 
  *
- * <Add Extended Description Here>
+ * @param temp   A unsigned char pointer to an n-element data array 
+ * @param size   An unsigned integer as the size of the array
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
  *
- * @return <Add Return Informaiton here>
- */
+ * @return void
+ *
+ *************************************************************/
+void print_array(unsigned char * temp, int size);
 
+/**************************************************************
+ * @brief Prints maximum, minimum, median and Mean to the screen
+ *		  Takes an array, size, all the statistics as input. 
+ *
+ * @param temp   A unsigned char pointer to an n-element data array 
+ * @param size   An unsigned integer as the size of the array
+ * @param maximum An integer which has the maximum value of the array
+ * @param minimum An integer which has the minimum value of the array
+ * @param mean An integer which has the Mean value of the array
+ * @param median An integer which has the median value of the array
+ * @return void
+ *
+ *************************************************************/
+void print_statistics(unsigned char * temp, int size, unsigned char maximum,
+						 unsigned char minimum, unsigned char average, unsigned char median); 
 
+/**************************************************************
+ * @brief Calculates the maximum value of the array
+ 		Takes an array and the size as input
+ *
+ * @param temp   A unsigned char pointer to an n-element data array 
+ * @param size   An unsigned integer as the size of the array
+ 
+ * @return An integer, maximum value of the array.
+ *
+ *************************************************************/
+unsigned char find_maximum(unsigned char * temp, int size);
+
+/**************************************************************
+ * @brief Calculates the minimum value of the array
+ 		Takes an array and the size as input
+ *
+ * @param temp   A unsigned char pointer to an n-element data array 
+ * @param size   An unsigned integer as the size of the array
+ 
+ * @return An integer, minimum value of the array.
+ *
+ *************************************************************/
+unsigned char find_minimum(unsigned char * temp, int size);
+
+/**************************************************************
+ * @brief Calculates the mean of the array
+ 		Takes an array and the size as input
+ *
+ * @param temp   A unsigned char pointer to an n-element data array 
+ * @param size   An unsigned integer as the size of the array
+ 
+ * @return an integer, mean of the array.
+ *
+ *************************************************************/
+unsigned char find_mean(unsigned char * temp, int size);
+
+/**************************************************************
+ * @brief Calculates the median of the array
+ 		Takes an array and the size as input
+ *
+ * @param temp   A unsigned char pointer to an n-element data array 
+ * @param size   An unsigned integer as the size of the array
+ 
+ * @return an integer, median of the array.
+ *
+ *************************************************************/
+unsigned char find_median(unsigned char * temp, int size);
+
+/**************************************************************
+ * @brief Sorts the array, in non increasing values. 
+ 		Takes an array and the size as input
+ *
+ * @param temp   A unsigned char pointer to an n-element data array 
+ * @param size   An unsigned integer as the size of the array
+ 
+ * @return void
+ *
+ *************************************************************/
+void sort_array(unsigned char * temp, int size);
 #endif /* __STATS_H__ */
