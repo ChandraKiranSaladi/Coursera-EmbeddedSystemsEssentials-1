0de47805cb46d45ffb26f275edd8421ced8f60a5
Chandra Kiran Saladi <kiranck18@gmail.com>
2018-06-14 21:01:32 -0500

    Creating Initial Coursera Repository

---
+++ README.md
@@ -0,0 +1,4 @@
+AUTHOR: Chandra Kiran Saladi
+Date : 06/14/2018
+
+Assignment 1: Embedded Software Essentials. 
---
+++ stats.h
@@ -0,0 +1,47 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+
+ *****************************************************************************/
+/**
+ * @file stats.h 
+ * @brief Simple Program to print the statistics of the given data such as 
+ 					- Maximum
+ 					- Minimum
+ 					- Mean
+ 					- Median
+ *
+ *
+ * @author Chandra Kiran Saladi
+ * @date 06/13/2018
+ *
+ */
+
+
+
+#ifndef __STATS_H__
+#define __STATS_H__
+
+/* Add Your Declarations and Function Comments here */ 
+
+/**
+ * @brief <Add Brief Description of Function Here>
+ *
+ * <Add Extended Description Here>
+ *
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ *
+ * @return <Add Return Informaiton here>
+ */
+
+
+#endif /* __STATS_H__ */
